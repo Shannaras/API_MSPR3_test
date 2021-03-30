9df113150303255624bc11569f12855fec6e7094
Shannaras <44708608+Shannaras@users.noreply.github.com>
2021-03-30 14:26:54 +0200

    Création du DELETE

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get("/recuperationXML",(req,res) => {
   res.status(200).json(fichiersXML)
 })
 
+//Récupérer le dernier fichier XML
+app.get("/recuperationXML_latest",(req,res) => {
+  res.status(200).json(fichiersXML[fichiersXML.length-1])
+})
+
+
 //Récupérer un fichier XML précis
 app.get("/recuperationXML/:id",(req,res) => {
   const id = parseInt(req.params.id)
@@ -17,10 +23,22 @@ app.get("/recuperationXML/:id",(req,res) => {
   res.status(200).json(fichierXML)
 })
 
-//Envoie un fichier XML
+
+//Envoie le contenu du fichier XML au format JSON
+// params: body
 app.post("/envoieXML", (req,res) => {
-  fichiersXML.push(req.body)
+  var idCurrent = (fichiersXML[fichiersXML.length-1].id) +1
+  var jsonToAdd = {"id": idCurrent,"body":req.body.body}
+  fichiersXML.push(jsonToAdd)
   res.status(200).json(fichiersXML)
+})
+
+//Effacer un fichier XML
+app.delete('/supressionXML/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    let fichierXML = fichiersXML.find(fichierXML => fichierXML.id === id)
+    fichiersXML.splice(fichiersXML.indexOf(fichierXML),1)
+    res.status(200).json(fichiersXML)
 })
 
 
