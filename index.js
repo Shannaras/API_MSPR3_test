48331b6f2107a0b75bbcd60299dcaa6b47133701
Shannaras <44708608+Shannaras@users.noreply.github.com>
2021-03-30 13:19:10 +0200

    création fichier.json init du get et post dans index.js

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const app = express()
 app.listen(8080, () => {
   console.log("Serveur à l'écoute")
 })
+
+//Récupérer un fichier XML
+app.get("/fichierXML",(req,res) => {
+    res.send("Fichier XML récupéré")
+})
