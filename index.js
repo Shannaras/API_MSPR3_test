d33c24ef1a06fd0bf981cd68f3b8d4e1b20dd399
Shannaras <44708608+Shannaras@users.noreply.github.com>
2021-03-30 13:45:19 +0200

    modification de fichierXML.json création du get "un fichier precis" création du post "envoie d'un fichier"

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
+const fichiersXML = require('./fichierXML.json')
+
+app.use(express.json())
+
+//Récupérer tous les fichiers XML
+app.get("/recuperationXML",(req,res) => {
+  //res.send("Fichier XML récupéré")
+  res.status(200).json(fichiersXML)
+})
+
+//Récupérer un fichier XML précis
+app.get("/recuperationXML/:id",(req,res) => {
+  const id = parseInt(req.params.id)
+  const fichierXML = fichiersXML.find(fichierXML => fichierXML.id === id)
+  res.status(200).json(fichierXML)
+})
+
+//Envoie un fichier XML
+app.post("/envoieXML", (req,res) => {
+  fichiersXML.push(req.body)
+  res.status(200).json(fichiersXML)
+})
+
 
 app.listen(8080, () => {
   console.log("Serveur à l'écoute")
-})
-
-//Récupérer un fichier XML
-app.get("/fichierXML",(req,res) => {
-    res.send("Fichier XML récupéré")
 })
